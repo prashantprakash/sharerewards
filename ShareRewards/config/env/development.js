016ea8431dcb9225ca513ee68cbd128b83cf98c7
Prashant <prashantpacy@gmail.com>
2015-09-26 20:46:00 -0500

    sails mvc framework

--- ShareRewards/config/env/development.js
+++ ShareRewards/config/env/development.js
@@ -1,63 +1,24 @@
-'use strict';
+/**
+ * Development environment settings
+ *
+ * This file can include shared settings for a development team,
+ * such as API keys or remote database passwords.  If you're using
+ * a version control solution for your Sails app, this file will
+ * be committed to your repository unless you add it to your .gitignore
+ * file.  If your repository will be publicly viewable, don't add
+ * any private information to this file!
+ *
+ */
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
-  debug: true,
-  logging: {
-    format: 'tiny'
-  },
-  //  aggregate: 'whatever that is not false, because boolean false value turns aggregation off', //false
-  aggregate: false,
-  mongoose: {
-    debug: false
-  },
-  hostname: 'http://localhost:3000',
-  app: {
-    name: 'MEAN - A Modern Stack - Development'
-  },
-  strategies: {
-    local: {
-      enabled: true
-    },
-    landingPage: '/',
-    facebook: {
-      clientID: 'DEFAULT_APP_ID',
-      clientSecret: 'APP_SECRET',
-      callbackURL: 'http://localhost:3000/api/auth/facebook/callback',
-      enabled: false
-    },
-    twitter: {
-      clientID: 'DEFAULT_CONSUMER_KEY',
-      clientSecret: 'CONSUMER_SECRET',
-      callbackURL: 'http://localhost:3000/api/auth/twitter/callback',
-      enabled: false
-    },
-    github: {
-      clientID: 'DEFAULT_APP_ID',
-      clientSecret: 'APP_SECRET',
-      callbackURL: 'http://localhost:3000/api/auth/github/callback',
-      enabled: false
-    },
-    google: {
-      clientID: 'DEFAULT_APP_ID',
-      clientSecret: 'APP_SECRET',
-      callbackURL: 'http://localhost:3000/api/auth/google/callback',
-      enabled: false
-    },
-    linkedin: {
-      clientID: 'DEFAULT_API_KEY',
-      clientSecret: 'SECRET_KEY',
-      callbackURL: 'http://localhost:3000/api/auth/linkedin/callback',
-      enabled: false
-    }
-  },
-  emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
-  mailer: {
-    service: 'SERVICE_PROVIDER', // Gmail, SMTP
-    auth: {
-      user: 'EMAIL_ID',
-      pass: 'PASSWORD'
-    }
-  }, 
-  secret: 'SOME_TOKEN_SECRET'
+
+  /***************************************************************************
+   * Set the default database connection for models in the development       *
+   * environment (see config/connections.js and config/models.js )           *
+   ***************************************************************************/
+
+  // models: {
+  //   connection: 'someMongodbServer'
+  // }
+
 };
